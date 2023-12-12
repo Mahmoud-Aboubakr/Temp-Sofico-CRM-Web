2450f59af52fdc6c5b9e88c0a8d97f82f958a9be
Mahmoud-Aboubakr <134690677+Mahmoud-Aboubakr@users.noreply.github.com>
2023-12-12 11:24:33 +0200

    Be b ma add items controller (#13)
    * add items interface, implementation and controller  that has filter method
    
    * edit the name of Stored Procedure
    
    ---------
    
    Co-authored-by: Muhammad Ali <35249585+MuhammadBnAly@users.noreply.github.com>

---
+++ SofiForce.Web/Dapper/Implementation/ItemManager.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using DocumentFormat.OpenXml.Bibliography;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Models;
+using SofiForce.BusinessObjects;
+using SofiForce.DataObjects;
+using SofiForce.Web.Dapper.Interface;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+
+namespace SofiForce.Web.Dapper.Implementation;
+
+public class ItemManager : IItemManager
+{
+	private readonly DapperContext _context;
+
+    public ItemManager(DapperContext context)
+    {
+        _context = context;
+    }
+
+
+    public List<ItemListModel> filter(ItemSearchModel searchModel)
+    {
+        List<ItemListModel> model = new List<ItemListModel>();
+        try
+		{
+            using (var connection = _context.CreateConnection())
+            {
+                var param = new
+                {
+                    @ItemId = searchModel.ItemId,
+                    @ItemName = searchModel.ItemName,
+                    @ItemCode = searchModel.ItemCode,
+                    @VendorId = searchModel.VendorId,
+                    @PublicPrice = searchModel.PublicPrice,
+                    @ClientPrice = searchModel.ClientPrice,
+                    @IsNewAdded = searchModel.IsNewAdded == 0 ? "" : searchModel.IsNewAdded.ToString(),
+                    @HasPromotion = searchModel.HasPromotion,
+                    @IsActive = searchModel.IsActive == 0 ? "" : searchModel.IsActive.ToString(),
+                    @IsNewStocked = searchModel.IsNewStocked == 0 ? "" : searchModel.IsNewStocked.ToString(),
+                    @Skip = searchModel.Skip,
+                    @Take = searchModel.Take,
+                    @Term = searchModel.Term,
+                    @FilterBy = searchModel.FilterBy,
+                    @StoreId = searchModel.StoreId,
+                    @SortBy = searchModel.SortBy,
+                    @TermBy = searchModel.TermBy,
+                };
+                model = connection.Query<ItemListModel>
+                    ("GetItemWithLatestPromotion", // storeprocedure here
+                    param, commandType: System.Data.CommandType.StoredProcedure
+                    ).ToList();
+
+            }
+        }
+		catch (System.Exception)
+		{
+
+			throw;
+		}
+        return model;
+    }
+
+
+}
